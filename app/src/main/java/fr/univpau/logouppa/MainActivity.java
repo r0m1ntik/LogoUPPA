88ae659402dca73da28483597d0d2065b9bab0b1
r0m1ntik <r.badanin94@yandex.ru>
2022-11-21 18:25:28 +0100

    Initial commit

---
+++ app/src/main/java/fr/univpau/logouppa/MainActivity.java
@@ -0,0 +1,46 @@
+package fr.univpau.logouppa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (isNetworkAvailable(getApplication())) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.univ-pau.fr/"));
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "Aucune connection internet !", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // si la personne est connecté a internet ou pas
+    private Boolean isNetworkAvailable(Application application) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) application.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network nw = connectivityManager.getActiveNetwork();
+        if (nw == null) return false;
+        NetworkCapabilities actNw = connectivityManager.getNetworkCapabilities(nw);
+        return actNw != null && (actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH));
+    }
+}
